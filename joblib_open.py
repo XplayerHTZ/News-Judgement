72697764e54992e375cdc89361bf862e50c0f586
XplayerHTZ <xplayerhtz123@outlook.com>
2024-12-15 15:35:16 +0800

    add streamlit application

--- joblib_open.py
+++ joblib_open.py
@@ -1,8 +1,37 @@
-# 加载并使用模型
-from joblib import load
+'''
+加载并使用模型
+'''
+from joblib import load # 加载模型
+import pandas as pd # Pandas数据处理
+from nltk.stem.porter import PorterStemmer # 词干提取器
+import re # 正则表达式
+from sklearn.metrics import accuracy_score # 准确率
 
-loaded_best_model = load('best_model.joblib')
-new_text = ["This is a sample news article to test the model."] #  示例新文本
-new_text_transformed = loaded_best_model.named_steps['tfidf'].transform(new_text) # 使用训练时的tfidf转换器
-prediction = loaded_best_model.predict(new_text_transformed)
-print(prediction) # 输出预测结果
+# 读取文件
+def read_text():
+    with open('input_text.txt', 'r' , encoding = "utf-8") as f:
+        text = f.read()
+    return text
+
+text = read_text()
+
+# PorterStemmer词干提取
+def stemming(content):
+    stemmer = PorterStemmer()
+    stemmed_content = re.sub('[^a-zA-Z]',' ',content) # 使用正则表达式将所有非字母字符替换为空格
+    stemmed_content = stemmed_content.lower() # 使用Python的lower()方法将所有字母转换为小写
+    stemmed_content = stemmed_content.split() # 使用Python的split()方法将文本分割为单词
+    stemmed_content = [stemmer.stem(word) for word in stemmed_content] # 使用PorterStemmer将单词提取词干
+    stemmed_content = ' '.join(stemmed_content) # 使用Python的join()方法将提取词干后的单词重新连接为文本
+    return stemmed_content
+
+text = stemming(text)
+
+
+# 加载并使用模型
+def load_model(input):
+    loaded_best_model = load('best_model.joblib')
+    prediction = loaded_best_model.predict(pd.Series(input))
+    return prediction
+
+prediction = load_model(text)
---
+++ streamlit.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import joblib_open
+import pandas as pd
+
+st.markdown("# Simple demo for News-Judgement")
+
+label = None
+text = None
+
+with st.form(key="form_input"):
+    st.write("Please directly input the text")
+    label = st.text_input("Label")
+    text = st.text_input("Text")
+    submit = st.form_submit_button("Submit")
+
+def check_valid(label, text):
+    if label is None or text is None:
+        return False
+    if label == "" or text == "":
+        return False
+    return True
+
+if submit:
+    if check_valid(label, text):
+        combined_text = label + " " + text
+        with open("input_text.txt", "w" ,encoding = "utf-8") as f:
+            f.write(combined_text)
+    else:
+        st.warning("Please input the valid label and text")
+
+text = joblib_open.read_text()
+text = joblib_open.stemming(text)
+prediction = joblib_open.load_model(text)
+
+if prediction == 1:
+    prediction = "Real"
+else:
+    prediction = "Fake"
+
+st.write("The prediction is: ", prediction)
